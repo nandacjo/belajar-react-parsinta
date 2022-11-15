3dd4b357c3a823ced337c1d0c3e5d1147c6dd344
zeno <zeno@pop-os.localdomain>
2022-11-15 17:23:10 +0700

    function nesting component

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,64 @@
-import {
-    IconBrandFacebook,
-    IconBrandGithub,
-    IconBrandTwitter,
-} from '@tabler/icons';
-import clsx from 'clsx';
+import { IconBrandFacebook, IconBrandGithub, IconBrandTwitter } from '@tabler/icons';
+import Button from './components/Button';
+import Card from './components/Card';
 
 export default function App() {
     const type = 'submit';
     const onClick = () => console.log('Register with another style..');
 
     return (
-        <div className='text-3xl bg-slate-900 grid place-content-center min-h-screen font-bold'>
-            <div className='flex gap-x-2 max-w-xs flex-wrap gap-y-2'>
-                <Button className='bg-green-700' text='Google' />
-
-                <Button {...{ type, onClick }}>
-                    <IconBrandTwitter />
-                    Register
-                </Button>
-                <Button onClick={() => console.log('Register')}>
-                    <IconBrandTwitter />
-                    Register
-                </Button>
-                <Button
-                    className='bg-sky-600'
-                    onClick={() => console.log('Login')}
-                    type='submit'
-                >
-                    <IconBrandFacebook />
-                    Login
-                </Button>
-
-                <Button
-                    className='bg-black'
-                    onClick={() => console.log('Github')}
-                    type='submit'
-                >
-                    <IconBrandGithub />
-                    Github
-                </Button>
+        <div className=' bg-slate-100 text-slate-800 tracking-tight antialiased flex items-center justify-center min-h-screen'>
+            <div class='max-w-2xl flex items-center gap-x-4 w-full'>
+                {/* <Card title={'Hello World'} footer={<Button>Register</Button>}> */}
+                <Card>
+                    <Card.Title>Hello Bangke</Card.Title>
+                    <Card.Body>
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam rerum dicta maxime ea, laborum inventore commodi minus
+                            quae non eum aut nulla eos alias dolorum cumque mollitia unde velit fugiat!
+                        </p>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Button>Regiter</Button>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Title>Hello Bangke</Card.Title>
+                    <Card.Body>
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam rerum dicta maxime ea, laborum inventore commodi minus
+                            quae non eum aut nulla eos alias dolorum cumque mollitia unde velit fugiat!
+                        </p>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Button>Regiter</Button>
+                    </Card.Footer>
+                </Card>
             </div>
         </div>
     );
 }
 
-function Button(props) {
-    const {
-        className = 'bg-blue-600',
-        children,
-        text,
-        type = 'submit',
-    } = props;
-    return (
-        // <button {...props} className={`${className} [&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2  text-white px-4 py-2 rounded`}>
-        //     {text || children}
-        // </button>
+// const Title = () => (
+//     <>
+//         <h1>Hello World</h1>
+//         <h2>Hello Satrunys</h2>
+//     </>
+// )
 
-        // menggunakan plugin clsx
-        <button
-            {...props}
-            type={type}
-            className={clsx(
-                className,
-                '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2  text-white px-4 py-2 rounded'
-            )}
-        >
-            {text || children}
-        </button>
-    );
-}
+// const Button = (props) => {
+//     const { className = 'bg-blue-600', children, text, type = 'submit' } = props;
+//     return (
+//         // <button {...props} className={`${className} [&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2  text-white px-4 py-2 rounded`}>
+//         //     {text || children}
+//         // </button>
+
+//         // menggunakan plugin clsx
+//         <button
+//             {...props}
+//             type={type}
+//             className={clsx(className, '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2  text-white px-4 py-2 rounded')}>
+//             {text || children}
+//         </button>
+//     );
+// }
